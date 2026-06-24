3000abcbbaeecc7a409571584a59aa6ac88429f8
er1cyao <104165512+er1cyao@users.noreply.github.com>
2026-06-24 12:36:34 -0400

    Add: baseball-game - submission-2

---
+++ Data Structures & Algorithms/baseball-game/submission-2.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int out = 0;
+        int first = 0, second = 0;;
+
+        for(int i = 0; i < operations.length; i++){
+            if(operations[i].equals("+")){
+                first = stack.pop();
+                second = stack.peek();
+                stack.push(first);
+                stack.push(second + first);
+            }
+            else if(operations[i].equals("C")){
+                stack.pop();
+            }
+            else if(operations[i].equals("D")){
+                stack.push(stack.peek()*2);
+            }
+            else{
+                stack.push(Integer.parseInt(operations[i]));
+            }
+        }
+        for(int num : stack){
+            out+= num;
+        }
+        return out;
+    }
+}
